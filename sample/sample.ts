02354cce2ed4e89f16f23f5751ef4589776a625e
Koichi Suematsu <pascal.jp@gmail.com>
2021-04-11 06:57:17 +0900

    Add a sample script to illustlate how to use this library.

---
+++ sample/sample.ts
@@ -0,0 +1,34 @@
+import { Configuration } from "../ts/genfiles/configuration";
+import * as Soracom from "../ts/genfiles/api";
+
+async function main() {
+  const authApi = new Soracom.AuthApi();
+  const authResponse: Soracom.AuthResponse = (
+    await authApi.auth({
+      email: process.env.SORACOM_EMAIL,
+      password: process.env.SORACOM_PASSWORD,
+    })
+  ).data;
+
+  const auth = new Configuration({
+    apiKey: (name: string): string => {
+      if (name == "X-Soracom-API-Key" && authResponse.apiKey) {
+        return authResponse.apiKey;
+      } else if (name == "X-Soracom-Token" && authResponse.token) {
+        return authResponse.token;
+      } else {
+        return "";
+      }
+    },
+  });
+
+  try {
+    const simApi = new Soracom.SimApi(auth);
+    const sims: Soracom.Sim[] = (await simApi.listSims()).data;
+    console.log(sims);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
